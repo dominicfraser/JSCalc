63d324a4c120442da6faaea94cbc8e30a60f4a3b
Marcus Lancaster <marcus.h.lancaster@gmail.com>
2018-01-09 20:37:50 +0000

    Create Calculator class and add method to add 10 to a running total.

--- calculator.js
+++ calculator.js
@@ -1,1 +1,14 @@
-console.log("Hiya Guys");
+class Calculator {
+    constructor() {
+        this.runningTotal = 0;
+    }
+
+    add() {
+        this.runningTotal += 10;
+        console.log(this.runningTotal);
+    }
+}
+
+const cal = new Calculator 
+document.getElementById('test').onclick = () => {cal.add()}
+// ()=>{} anonoymous function / next we add the function from the new instance of the calculator class.  if we add it without putting it in, it breaks... :(
