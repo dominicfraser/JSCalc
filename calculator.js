d6750afbe282f24d032e336e4792a037b05d4446
Dominic Fraser <mrdjfraser@gmail.com>
2018-01-13 17:09:37 +0000

    trialling using jsPrettier

--- calculator.js
+++ calculator.js
@@ -1,61 +1,65 @@
 class Calculator {
-    constructor() {
-        this.displayedTotal = 0 //main display
-        // this.fullOperationDisplay = 0 //second smaller display
-        this.runningTotal = 0
-        this.pendingOperator = false
-        this.firstEntry = true
-        this.currentEntry = ""
-    }
+  constructor() {
+    this.displayedTotal = 0 //main display
+    // this.fullOperationDisplay = 0 //second smaller display
+    this.runningTotal = 0
+    this.pendingOperator = false
+    this.firstEntry = true
+    this.currentEntry = ""
+  }
 
-    selectNumber(key){
-        this.currentEntry += key //concats it to string for multiple numbers
-console.log('currentEntry', this.currentEntry)
-    }
+  selectNumber(key) {
+    this.currentEntry += key //concats it to string for multiple numbers
+    console.log("currentEntry", this.currentEntry)
+  }
 
-    selectOperand(op){
-      if (this.firstEntry === true){ 
+  selectOperand(op) {
+    if (this.firstEntry === true) {
       //if its the first number then once a operator is pressed it automatically becomes the running total once an operand press signals that the number entry is complete
+      this.pendingOperator = op
+      this.runningTotal += parseFloat(this.currentEntry) //we've been concatenating strings
+      this.currentEntry = "" //ready for number two
+      this.firstEntry = false //only to reset on CE or AC(?)
+      console.log("runningTotal", this.runningTotal)
+      console.log("currentEntry", this.currentEntry)
+    } else {
+      if (op !== "=") {
+        // if its the not the first time the operand has been pressed then start the operation, unless it is = in which case update the display
+        this.performOperation()
         this.pendingOperator = op
-        this.runningTotal += parseFloat(this.currentEntry) //we've been concatenating strings
-        this.currentEntry = "" //ready for number two
-        this.firstEntry = false //only to reset on CE or AC(?)
-console.log('runningTotal', this.runningTotal)
-console.log('currentEntry', this. currentEntry)
       } else {
-        if (op !== "="){ // if its the not the first time the operand has been pressed then start the operation, unless it is = in which case update the display
-          this.performOperation()
-          this.pendingOperator = op
-        } else {
-          this.displayedTotal = this.runningTotal += parseFloat(this.currentEntry)
-console.log('displayedTotal', this.displayedTotal)
-        }
+        this.displayedTotal = this.runningTotal += parseFloat(this.currentEntry)
+        console.log("displayedTotal", this.displayedTotal)
       }
-
     }
+  }
 
-    performOperation(){
-        switch (this.pendingOperator){
-          case "+":
-            this.runningTotal += parseFloat(this.currentEntry)
-            this.currentEntry = ""
-console.log('runningTotal', this.runningTotal)
-console.log('currentEntry', this. currentEntry)
-            break
-          // case "-"
-          //   break
-          // case "*"
-          //   break
-          // case "/"
-          //   break
-        }
+  performOperation() {
+    switch (this.pendingOperator) {
+      case "+":
+        this.runningTotal += parseFloat(this.currentEntry)
+        this.currentEntry = ""
+        console.log("runningTotal", this.runningTotal)
+        console.log("currentEntry", this.currentEntry)
+        break
+      // case "-"
+      //   break
+      // case "*"
+      //   break
+      // case "/"
+      //   break
     }
-
+  }
 }
 
-const cal = new Calculator 
+const cal = new Calculator()
 
-document.getElementById('6-btn').onclick = () => {cal.selectNumber("6")}
-document.getElementById('+-btn').onclick = () => {cal.selectOperand("+")}
-document.getElementById('=-btn').onclick = () => {cal.selectOperand("=")}
-
+document.getElementById("6-btn").onclick = () => {
+  cal.selectNumber("6")
+}
+document.getElementById("+-btn").onclick = () => {
+  cal.selectOperand("+")
+}
+document.getElementById("=-btn").onclick = () => {
+  cal.selectOperand("=")
+}
