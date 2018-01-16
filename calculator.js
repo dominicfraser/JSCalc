4095d253742fe60685aa58ecf8c5054270a093ec
Dominic Fraser <mrdjfraser@gmail.com>
2018-01-16 19:38:06 +0000

    removed code to a runner file

--- calculator.js
+++ calculator.js
@@ -51,15 +51,3 @@ class Calculator {
     }
   }
 }
-
-const cal = new Calculator()
-
-document.getElementById("6-btn").onclick = () => {
-  cal.selectNumber("6")
-}
-document.getElementById("+-btn").onclick = () => {
-  cal.selectOperand("+")
-}
-document.getElementById("=-btn").onclick = () => {
-  cal.selectOperand("=")
-}
